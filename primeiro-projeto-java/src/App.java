337333f8b9bdf886671a047604203909010fa342
Alexandra <Alexandra.Nascimento-ext@ab-Inbev.com>
2023-01-24 19:48:22 -0300

    nova atualizacao

--- primeiro-projeto-java/src/App.java
+++ primeiro-projeto-java/src/App.java
@@ -1,5 +1,13 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Vamos começar esses estudos com tudo");
+    public static void main (String[] args) {
+        
+        String primeiroNome = "Alexandra";
+        String segundoNome = "Nascimento";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
     }
 }
