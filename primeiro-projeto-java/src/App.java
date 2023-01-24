fb34458fe29108b4d23d75f6bf2cf7dc19291069
Alexandra <Alexandra.Nascimento-ext@ab-Inbev.com>
2023-01-24 19:09:17 -0300

    Atualizacao

--- primeiro-projeto-java/src/App.java
+++ primeiro-projeto-java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Vamos começar esses estudos com tudo");
     }
 }
